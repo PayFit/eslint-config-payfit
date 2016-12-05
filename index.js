5fd0a54de0c1208db92c174e8bcbefefe7a45570
ebourmalo <emmanuel.bourmalo@gmail.com>
2016-12-05 17:24:14 +0100

    Fix conventions and update deps

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = {
-  "parser": "babel-eslint",
   "extends": "airbnb",
+  "parser": "babel-eslint",
   "plugins": [
     "react",
     "jsx-a11y"
   ],
   "ecmaFeatures": {
     "jsx": true,
-    "spread": true,
+    "spread": true
   },
   "rules": {
     "no-use-before-define": [2, "nofunc"],
@@ -26,22 +26,18 @@ module.exports = {
     "quotes": [2, "single", {"avoidEscape": true, "allowTemplateLiterals": true}],
     "no-underscore-dangle": ["error", { "allow": ["_extend", "_id", "__super__"] }],
     "space-before-function-paren": ["error", "never"],
-
-    # custom
     "global-require": 0,
-    "import/no-unresolved": 0, # avoid errors when using webpacks' aliases
+    "import/no-unresolved": 0,
     "no-confusing-arrow": ["error", {"allowParens": true}],
     "no-console": 1,
     "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 1 }],
     "import/newline-after-import": 0,
     "import/no-extraneous-dependencies": 0,
     "import/prefer-default-export": 0,
-
-    # React / JSX
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "react/jsx-first-prop-new-line": 0,
     "react/forbid-prop-types": 0,
-    "react/no-unused-prop-types": 0, # useful but buggy atm
+    "react/no-unused-prop-types": 0,
     "jsx-quotes": ["error", "prefer-single"],
     "jsx-a11y/no-static-element-interactions": 0
   },
