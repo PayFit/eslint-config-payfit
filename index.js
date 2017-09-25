b5f5e963db1d738f15326dccfb20b5d61d8e8326
Yann Pringault <yann.pringault@gmail.com>
2017-09-25 14:12:59 +0200

    Update Airbnb's eslint config (#4)
    * Update Airbnb's eslint config
    
    * Upgrade deps

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 module.exports = {
   "extends": "airbnb",
   "parser": "babel-eslint",
-  "plugins": [
-    "react",
-    "jsx-a11y"
-  ],
-  "ecmaFeatures": {
-    "jsx": true,
-    "spread": true
-  },
   "rules": {
     "no-use-before-define": [2, "nofunc"],
     "max-len": [2, 100, 4],
