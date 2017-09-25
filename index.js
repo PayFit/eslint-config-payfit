32344dea4c44d17ef3dbb0deb5e67f550f9ad965
Yann Pringault <yann.pringault@gmail.com>
2017-09-25 15:06:19 +0200

    Rewrite numbers to strings for rules configuration

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ module.exports = {
   "extends": "airbnb",
   "parser": "babel-eslint",
   "rules": {
-    "comma-dangle": 0,
-    "semi": [2, "never"],
+    "comma-dangle": "off",
+    "semi": ["error", "never"],
     "no-underscore-dangle": ["error", { "allow": ["_extend", "_id", "__super__", "_source"] }],
     "jsx-quotes": ["error", "prefer-single"],
-    "jsx-a11y/no-static-element-interactions": 0,
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "react/forbid-prop-types": 0
+    "jsx-a11y/no-static-element-interactions": "off",
+    "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }],
+    "react/forbid-prop-types": "off"
   },
   "env": {
     "browser": true,
