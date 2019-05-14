39f2fc7c8ebd22d899f27d3d152c0568a2704c28
Toufik Airane <toufik.airane@payfit.com>
2019-05-14 16:13:36 +0200

    Add security rules (#8)
    * add lint script
    
    * go
    
    * go
    
    * go
    
    * go
    
    * go
    
    * go
    
    * 1.1.0-security.6
    
    * 1.1.0-security.7
    
    * go
    
    * 1.1.0-security.8
    
    * Go
    
    * go
    
    * 1.2.0-security.0
    
    * go
    
    * Rebase to rules
    
    * 1.1.0
    
    * 1.1.0-security.8
    
    * 1.1.0-security.0
    
    * 1.1.0-security.8
    
    * 1.1.0-security.9
    
    * Update README.md
    
    Co-Authored-By: toufik-airane <toufik.airane@payfit.com>
    
    * Fix minor code style
    
    * Fix minor issues
    
    * Fix issue
    
    * Fix yarn.lock
    
    * Version 1.2.0-security.0
    
    * 1.2.0-security.1
    
    * Update security.js
    
    * Update security.js
    
    * Update security.js
    
    * Go
    
    * Go
    
    
    Co-authored-by: Yann Pringault <yann.pringault@gmail.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+## eslint-config-payfit
+
+#### Edit .eslintrc
+
+```
+"extends": ["payfit", "payfit/security"],
+```
+
+#### Dependencies
+
+```
+npx install-peerdeps --dev eslint-config-payfit
+```
---
+++ rules/security.js
@@ -0,0 +1,47 @@
+module.exports = {
+  plugins: ['sonarjs', 'security', 'no-unsanitized', 'xss'],
+  extends: ['plugin:sonarjs/recommended', 'plugin:security/recommended'],
+  rules: {
+    // Cross-Site Scripting
+    'xss/no-location-href-assign': 'warn', // need to specify an escape function
+    'no-unsanitized/method': 'warn',
+    'no-unsanitized/property': 'warn',
+
+    // Bug Detection
+    'sonarjs/no-all-duplicated-branches': 'warn',
+    'sonarjs/no-element-overwrite': 'warn',
+    'sonarjs/no-extra-arguments': 'warn',
+    'sonarjs/no-identical-conditions': 'warn',
+    'sonarjs/no-identical-expressions': 'warn',
+    'sonarjs/no-one-iteration-loop': 'warn',
+    'sonarjs/no-use-of-empty-return-value': 'warn',
+    'security/detect-unsafe-regex': 'warn',
+    'security/detect-buffer-noassert': 'warn',
+    'security/detect-child-process': 'warn',
+    'security/detect-disable-mustache-escape': 'warn',
+    'security/detect-eval-with-expression': 'error',
+    'security/detect-no-csrf-before-method-override': 'warn',
+    'security/detect-non-literal-fs-filename': 'warn',
+    'security/detect-non-literal-regexp': 'warn',
+    'security/detect-non-literal-require': 'warn',
+    'security/detect-object-injection': 'off',
+    'security/detect-possible-timing-attacks': 'off',
+    'security/detect-pseudoRandomBytes': 'warn',
+    'security/detect-new-buffer': 'warn',
+
+    // Code Smell Detection
+    'sonarjs/cognitive-complexity': ['warn', 90],
+    'sonarjs/max-switch-cases': 'warn',
+    'sonarjs/no-duplicate-string': 'off',
+    'sonarjs/no-duplicated-branches': 'warn',
+    'sonarjs/no-identical-functions': 'off',
+    'sonarjs/no-inverted-boolean-check': 'warn',
+    'sonarjs/no-redundant-boolean': 'warn',
+    'sonarjs/no-small-switch': 'warn',
+    'sonarjs/no-useless-catch': 'warn',
+    'sonarjs/prefer-immediate-return': 'off',
+    'sonarjs/prefer-object-literal': 'warn',
+    'sonarjs/prefer-single-boolean-return': 'warn',
+    'sonarjs/prefer-while': 'warn',
+  },
+}
